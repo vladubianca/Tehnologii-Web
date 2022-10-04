3c0234143902ef06fd7d4f50b8e63625d2485878
vladubianca <vladubianca20@stud.ase.ro>
2022-10-04 17:00:32 +0300

    added all files

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-var http = require("http");
+// var http = require("http");
 
-// create a server object
-http
-  .createServer(function (req, res) {
-    res.write("Hello world!");
-    res.end();
-  })
-  .listen(8085);
+// // create a server object
+// http
+//   .createServer(function (req, res) {
+//     res.write("Hello world!");
+//     res.end();
+//   })
+//   .listen(8085);
+
+//Server Express
+// const express = require("express");
+// const req = require("express/lib/request");
+
+// const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("Hello world");
+// });
+
+// app.listen(8085);
+
+//Server Express cu continut static
+const express = require("express");
+
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(8085);
